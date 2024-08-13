7f3199eff03956ec7a014bc3aa98aa4ea71b5b99
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2024-08-12 21:24:49 -0300

    fix: correção texto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -99,7 +99,7 @@ const Home = async () => {
           </div>
 
           <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
-            Agendamentos
+            Agendamento
           </h2>
 
           {/* AGENDAMENTO */}
